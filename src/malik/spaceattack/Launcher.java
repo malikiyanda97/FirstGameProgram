58ef9d4e0ee7fc8b3e587424f2204164fc79ef1e
Malik <miyanda97@gmail.com>
2018-02-14 18:28:50 +0000

    changed title of the game

--- src/malik/spaceattack/Launcher.java
+++ src/malik/spaceattack/Launcher.java
@@ -15,7 +15,7 @@ public class Launcher {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Game game = new Game("SpaceAttack",1100,800);
+        Game game = new Game("2D-FYP-Game",1100,800);
         game.start();
                 
                 
