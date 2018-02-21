8c9a42992519cb0269394d90ea8329fadc95e3ae
Malik <miyanda97@gmail.com>
2018-02-21 01:30:05 +0000

    renaming done

---
+++ src/Survivor/Camera.java
@@ -0,0 +1,56 @@
+package malik.survivo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import malik.survivor.entitiesManager.Entities;
+
+/**
+ *
+ * @author miyan
+ */
+public class Camera {
+    
+    private float x, y;
+    
+    public Camera(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void tick(Entities entity){
+        x += ((entity.getX() -x ) -  1110/2)*0.05f;
+        x += ((entity.getY() -y ) -  800/2)*0.05f;
+        
+        if(x <= 0 ) x = 0;
+        if(x >= 1032) x = 1032;
+        if(y <= 0) y = 0;
+        if(y >= 800) y = 800;
+    }
+    
+    
+    //GETTERS AND SETTERS 
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+
+    
+}
